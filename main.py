5a1f0c2db87bc009e11ea0ed922a24e5686cab19
Musoxon Qaraxujayev <musoxonqaraxujayev@gmail.com>
2022-12-11 22:33:42 +0500

    committed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import moviepy.editor
 from pathlib import Path
 
-video_file = Path('source.mp4')
+video_file = Path('test/source.mp4')
 
 video = moviepy.editor.VideoFileClip(f'{video_file}')
 audio = video.audio
-audio.write_audiofile(f'{video_file.stem}.mp3')
+audio.write_audiofile(f'test/{video_file.stem}.mp3')
